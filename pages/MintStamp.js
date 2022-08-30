5d220220f7fa39daa91bdfd606e31b977360361a
ThomasDang93 <thomasdang93@gmail.com>
2022-08-29 22:05:15 -0500

    Add &apos; on Mint Stamp to escape character

--- pages/MintStamp.js
+++ pages/MintStamp.js
@@ -180,7 +180,7 @@ function MintStamp() {
                     <div className="flex flex-wrap -mx-3 mb-6">
                         <div className="w-full px-3">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="description">
-                            A brief public bio or other text you'd want to appear with your stamp
+                            A brief public bio or other text you&apos;d want to appear with your stamp
                             </label>
                             <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" rows="4" id="description" name="description" type="textarea" placeholder="Enter instructions here" onChange={handleChange}/>
                         </div>
