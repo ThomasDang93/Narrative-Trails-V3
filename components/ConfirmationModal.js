090ec01fab8405a7e691564dd711a79ce4c3c21c
ThomasDang93 <thomasdang93@gmail.com>
2022-11-04 21:14:17 -0500

    add test map

--- components/ConfirmationModal.js
+++ components/ConfirmationModal.js
@@ -39,7 +39,7 @@ const ConfirmationModal = ({show, onClose}) => {
                         </p>
                     </div>
                     <div className="items-center px-4 py-3">
-                        <a href="#" onClick={handleCloseClick} id="ok-btn" className="px-4 py-2 bg-green-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300" >
+                        <a href="#" onClick={handleCloseClick} id="ok-btn" className="px-4 py-2 bg-red-500 text-white text-base font-medium rounded-md w-full shadow-sm hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-300" >
                             Close
                         </a>
                     </div>
--- pages/pendingletterbox/[id].js
+++ pages/pendingletterbox/[id].js
@@ -252,7 +252,7 @@ const PendingLetterbox = ({ pendingLetterboxes }) => {
                             <textarea className="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" rows="4" id="description" name="description" type="textarea" placeholder="Enter instructions here" onChange={handleChange}/>
                         </div>
                     </div>
-                    <Map state={state}/>
+                    <Map state={state}/> 
                     <div className="flex flex-wrap -mx-3 mb-2">
                         <div className="w-full md:w-1/3 px-3 mb-6 md:mb-0">
                             <label className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" htmlFor="lattitude">
